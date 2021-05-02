2c490eed1144e0e51d15cba63ce7247c5a681724
RamanSB <ramansb100@gmail.com>
2021-05-02 18:19:50 +0100

    Backend fully functioning no erros when running backend server

--- Chapter 03/mern_skeleton/server/routes/user.routes.js
+++ Chapter 03/mern_skeleton/server/routes/user.routes.js
@@ -1,30 +1,18 @@
-import express from 'express';
-import userCtrl from '../controllers/user.controller';
-import authCrl from '../controllers/auth.controller';
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+import authCtrl from '../controllers/auth.controller'
 
-const router = express.Router(); //Used to create endpoints.
+const router = express.Router()
 
 router.route('/api/users')
-    .get(userCtrl.list)
-    .post(userCtrl.create);
+  .get(userCtrl.list)
+  .post(userCtrl.create)
 
 router.route('/api/users/:userId')
-    .get(authCtrl.requireSignIn, userCtrl.read)
-    .put(authCtrl.requireSignIn, authCtrl.hasAuthorization, userCtrl.update)
-    .delete(authCtrl.requireSignIn, authCtrl.hasAuthorization, userCtrl.remove);
+  .get(authCtrl.requireSignin, userCtrl.read)
+  .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+  .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove)
 
-/**
- * This implies that when a route/endpoint that contains the :userId parameter within
- * its path, the UserController will invoke the userByID function first, prior to executing
- * the specified controller function in the route definition.
- */
-router.param('userId', userCtrl.userByID); 
+router.param('userId', userCtrl.userByID)
 
-
-
-
-export default router;
-
-/**
- * MAKE NOTES ON PAPER ABOUT THE METHODS FROM MONGOOSE + METHODS FROM EXPRESS.
- */
+export default router
